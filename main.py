2b90701cc69f4afd9237d5569ccbef86cb73135c
abrech <adrian.brechtken@gmail.com>
2024-07-24 19:19:38 +0200

    added bubbles

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 600
 FPS = 60
 
+FISH_SPAWN_OFFSET_X = 128
+FISH_SPAWN_OFFSET_Y = 32
+
 # Colors
 WHITE = (255, 255, 255)
 
@@ -20,10 +23,17 @@ pygame.display.set_caption("Interactive Fish Tank")
 background = pygame.image.load('assets/background.png')
 
 # Fish animation frames
-fish_images = [
+pink_fish_images = [
     pygame.image.load('assets/pink_fish/pink_fish_1.png'),
     pygame.image.load('assets/pink_fish/pink_fish_2.png'),
     pygame.image.load('assets/pink_fish/pink_fish_3.png'),
+]
+
+orange_fish_images = [
+    pygame.image.load('assets/orange_fish/orange_fish_1.png'),
+    pygame.image.load('assets/orange_fish/orange_fish_2.png'),
+    pygame.image.load('assets/orange_fish/orange_fish_3.png'),
+    pygame.image.load('assets/orange_fish/orange_fish_4.png'),
 ]
 
 # bubble animation frames
@@ -31,46 +41,44 @@ bubble_images = [
     pygame.image.load('assets/bubbles/bubble_x4_1.png'),
     pygame.image.load('assets/bubbles/bubble_x4_2.png'),
 ]
+
+fish_images = [pink_fish_images, orange_fish_images]
 # Resize fish frames if necessary
-fish_images = [pygame.transform.scale(img, (64, 64)) for img in fish_images]
+fish_images = [[pygame.transform.scale(img, (64, 64)) for img in fishes] for fishes in fish_images]
 
 
 # Fish class
 class Fish(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x=None):
         super().__init__()
-        self.images = fish_images
+        self.from_right = bool(random.getrandbits(1))
+        self.images = random.choice(fish_images)
         self.image_index = 0
-        self.image = self.images[self.image_index]
-        self.rect = self.image.get_rect()
-        self.rect.x = random.randint(0, SCREEN_WIDTH - self.rect.width)
-        self.rect.y = random.randint(0, SCREEN_HEIGHT - self.rect.height)  # Ensure fish doesn't start in the ground
-        self.speed_x = random.choice([-1, 1])
-        self.speed_y = 0
         self.animation_speed = 0.1  # Adjust animation speed as needed
+        self.image = self._get_image()
+        self.rect = self.image.get_rect()
+        self.rect.x = x if x else (0 - self.rect.width if not self.from_right else SCREEN_WIDTH + self.rect.width)
+        self.rect.y = random.randint(0,
+                                     SCREEN_HEIGHT - self.rect.height - FISH_SPAWN_OFFSET_Y)  # Ensure fish doesn't start in the ground
+        self.speed_x = -1 if self.from_right else 1
+        self.speed_y = 0
 
-    def _bounce_edges(self):
-        if self.rect.left < 0 or self.rect.right > SCREEN_WIDTH:
-            self.speed_x = -self.speed_x
-            # flip the image
-            self.image = pygame.transform.flip(self.image, True, False)
-
-        if self.rect.top < 0 or self.rect.bottom > SCREEN_HEIGHT:
-            self.speed_y = -self.speed_y
-
-    def _update_animation(self):
+    def _get_image(self):
         self.image_index += self.animation_speed
         if self.image_index >= len(self.images):
             self.image_index = 0
-        self.image = self.images[int(self.image_index)]
+        image = self.images[int(self.image_index)]
+        if self.from_right:
+            image = pygame.transform.flip(image, True, False)
+        return image
+
+    def _update_animation(self):
+        self.image = self._get_image()
 
     def update(self):
         # Update position
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
-
-        # Bounce the fish off the edges
-        self._bounce_edges()
 
         # Update animation frame
         self._update_animation()
@@ -119,12 +127,14 @@ running = True
 clock = pygame.time.Clock()
 
 for _ in range(10):
-    fish = Fish()
+    fish = Fish(x=random.randint(FISH_SPAWN_OFFSET_X, SCREEN_WIDTH - FISH_SPAWN_OFFSET_X))
     fish_group.add(fish)
 
 while running:
     # remove sprites outside of screen
     bubbles_group.remove([b for b in bubbles_group if b.rect.y < 0])
+    fish_group.remove(
+        [b for b in fish_group if b.rect.x < 0 - 10 - b.rect.width or b.rect.x > SCREEN_WIDTH + 10 + b.rect.width])
 
     # randomly add bubbles
     if random.random() < 0.02:
@@ -132,6 +142,12 @@ while running:
         for i in range(random.randint(1, 5)):
             bubble = Bubble(x=x, y=SCREEN_HEIGHT - (i * 10))
             bubbles_group.add(bubble)
+
+    # randomly add fish
+
+    if random.random() < 1 - (len(fish_group) / 10):
+        fish = Fish()
+        fish_group.add(fish)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
